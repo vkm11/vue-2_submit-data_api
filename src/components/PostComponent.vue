99226cf991b7056967681dec6eacf89f96dfa18e
vijaykumar <vijay1111mane@gmail.com>
2022-07-08 14:29:23 +0530

    commit;

---
+++ src/components/PostComponent.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+      <form @submit="submitData" method="post">
+          <!-- <lable>Name: </lable> -->
+          <input type="text" name="title" v-model="post.title"/> <br> <br>
+          <input type="text" name="author" v-model="post.author" /> <br> <br>
+            <button type="submit">Post</button>
+      </form>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Post-component",
+    data()
+    {
+        return{
+            post:{
+                title:null,
+                author:null
+            }
+          
+        }
+    },
+    methods:{
+        submitData(e){
+            e.preventDefault()
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
